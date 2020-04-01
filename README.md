2eb8a512cfc0acedcf22e40e20bd38a00284539c
Jon Huffman <jon@jonhuffman.com>
2020-03-31 22:32:29 -0400

    Adding v0.1 of the Image Renderer tool.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# UnityImageRenderer
-A simple tool that allows you to render an image out of a scene
+[![version](https://img.shields.io/badge/package-download-brightgreen.svg)](./Packages/ImageRenderer_v0.1.unitypackage)
+[![version](https://img.shields.io/badge/version-v1.0-blue.svg)](https://github.com/jonHuffman/UnityImageRenderer)
+[![license](https://img.shields.io/badge/license-MIT-red.svg)](./LICENSE)
+# Image Renderer
+## Description
+This is a very simple "render to file" utility for Unity. Using this tool you are able to position a temporary camera in your scene and then save its output to an image file. Supported image formats are jpg and png, with alpha supported for pngs.
+
+## Features
+* Supports jpg and png file formats
+* Alpha channel supported for pngs
+* Supports image dimensions between 1 and 2048; Images can be non-square
+
+## How to Use
+### Opening the Tool
+The tool can be accessed from the Tools menu. Opening the tool will automatically create an ImageRendererCamera within your scene. Closing the Image Renderer tool will destroy the camera.
+
+![](./README/OpenCloseWindow.gif)
+
+### Staging your Image
+Once your ImageRendererCamera is created you are able to manipulate it as you would any other camera. Position your camera in the scene and stage the items you wish to render accordingly.
+
+![](./README/PositioningCamera.gif)
+
+### Resizing Image
+The image being captured can be resized by manipulating the width and height values within the Image Renderer window. The preview image will resize accordingly.
+
+![](./README/ResizingImage.gif)
+
+### Alpha Channel
+The Image Renderer supports alpha channel for pngs. To render your items on a transparent background simply change the Clear Flags of the ImageRendererCamera to "Depth Only". The Image Renderer window provides an alpha channel preview that can be toggled on/off as desired.
+
+![](./README/AlphaPreview.gif)
+
+## Known Issues
+* Alpha preview is available when the jpg format is selected
+* It is possible to delete the ImageRendererCamera while the Image Renderer window is open
+* Camera settings are not saved between closing and opening the Image Renderer window
+* The Image Renderer does not render transparency if post-processing is active on the camera
+** This appears to be a feature limitation of Unity
+
+## Meta
+Created by Jon Huffman [[twitter](https://twitter.com/AtticusMarkane) &bull; [github](https://github.com/jonHuffman)]
+
+Released under the [MIT License](http://www.opensource.org/licenses/mit-license.php).
---
+++ Unity.proj/Assets/ImageRenderer/Editor/ImageRendererWindow.cs
@@ -0,0 +1,281 @@
+﻿namespace ImageRenderer.Editor
+{
+    using System;
+    using System.IO;
+    using UnityEditor;
+    using UnityEngine;
+
+    public class ImageRendererWindow : EditorWindow
+    {
+        private const string SAVE_PATH_PREF_KEY = "ImageRendererWindow.SavePath";
+        private const string DEFAULT_FILENAME = "New Image";
+        private const int WINDOW_HORIZONTAL_BUFFER = 12;
+        private const int WINDOW_VERTICAL_BUFFER = 115;
+        private const int MAX_PREVIEW_DIMENSION = 500;
+        private const float TEXTURE_HORIZONTAL_OFFSET_SPLIT = 0.5f;
+        private const float TEXTURE_VERTICAL_OFFSET_SPLIT = 0.7f;
+
+        private Camera renderCamera;
+        private int textureWidth = 2048;
+        private int textureHeight = 2048;
+        private ImageFormat imageFormat = ImageFormat.png;
+
+        private RenderTexture renderTexture;
+        private bool showAlpha;
+
+        private string savePath = string.Empty;
+
+        private float ImageAspectRatio
+        {
+            get => (float)textureWidth / textureHeight;
+        }
+
+        #region Unity Methods
+
+        private void OnDestroy()
+        {
+            DestroyImmediate(renderCamera.gameObject);
+        }
+
+        private void OnGUI()
+        {
+            DrawImageSettings();
+            DrawImagePreview();
+            DrawSaveButtons();
+        }
+
+        private void OnFocus()
+        {
+            if (renderCamera != null)
+            {
+                RebuildRenderTarget();
+                Selection.activeGameObject = renderCamera.gameObject;
+            }
+        }
+
+        #endregion
+
+        [MenuItem("Tools/Image Renderer")]
+        private static void OpenWindow()
+        {
+            ImageRendererWindow window = GetWindow<ImageRendererWindow>(true, "Image Renderer", true);
+            Vector2 windowSize = new Vector2(MAX_PREVIEW_DIMENSION + WINDOW_HORIZONTAL_BUFFER, MAX_PREVIEW_DIMENSION + WINDOW_VERTICAL_BUFFER);
+            window.minSize = windowSize;
+            window.maxSize = windowSize;
+            window.Initialize();
+            window.Show();
+        }
+
+        private void Initialize()
+        {
+            if (renderCamera == null)
+            {
+                GameObject cameraObject = new GameObject("[TOOL] ImageRendererCamera", typeof(Camera));
+                cameraObject.hideFlags = HideFlags.DontSave;
+                renderCamera = cameraObject.GetComponent<Camera>();
+            }
+
+            Selection.activeGameObject = renderCamera.gameObject;
+            savePath = EditorPrefs.GetString(SAVE_PATH_PREF_KEY, String.Empty);
+
+            RebuildRenderTarget();
+        }
+
+        private void RebuildRenderTarget()
+        {
+            if (renderTexture != null)
+            {
+                renderCamera.targetTexture = null;
+                DestroyImmediate(renderTexture);
+            }
+
+            renderTexture = new RenderTexture(textureWidth, textureHeight, 32);
+            renderCamera.targetTexture = renderTexture;
+            renderCamera.aspect = ImageAspectRatio;
+
+            // Clears the active render target (our texture in this case)
+            GL.Clear(true, true, Color.clear);
+        }
+
+        private void DrawImageSettings()
+        {
+            const int MAX_TEXTURE_SIZE = 2048;
+
+            GUILayout.Label("Image Settings", EditorStyles.boldLabel);
+            using (new EditorGUI.IndentLevelScope())
+            {
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    using (EditorGUI.ChangeCheckScope check = new EditorGUI.ChangeCheckScope())
+                    {
+                        textureWidth = EditorGUILayout.IntField("Width", textureWidth);
+                        textureHeight = EditorGUILayout.IntField("Height", textureHeight);
+
+                        if (check.changed)
+                        {
+                            textureWidth = Mathf.Clamp(textureWidth, 1, MAX_TEXTURE_SIZE);
+                            textureHeight = Mathf.Clamp(textureHeight, 1, MAX_TEXTURE_SIZE);
+
+                            RebuildRenderTarget();
+                        }
+                    }
+                }
+
+                imageFormat = (ImageFormat)EditorGUILayout.EnumPopup("Image Format", imageFormat);
+            }
+        }
+
+        private void DrawSaveButtons()
+        {
+            const float SAVE_FIELDS_OFFSET = MAX_PREVIEW_DIMENSION - 15;
+            const float BUTTON_WIDTH = 100f;
+
+            bool selectPathClicked = false;
+            bool saveClicked = false;
+
+            GUILayout.Space(SAVE_FIELDS_OFFSET);
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                savePath = EditorGUILayout.TextField(savePath, GUILayout.ExpandWidth(true));
+                selectPathClicked = GUILayout.Button("Select Path", GUILayout.Width(BUTTON_WIDTH));
+                saveClicked = GUILayout.Button("Save", GUILayout.Width(BUTTON_WIDTH));
+            }
+
+            if (selectPathClicked)
+            {
+                savePath = EditorUtility.SaveFilePanel("Save Location", Application.dataPath, DEFAULT_FILENAME, imageFormat.ToString());
+            }
+
+            if (saveClicked)
+            {
+                Texture2D convertedTexture = ConvertToTexture2D(renderTexture);
+                SaveTexture(convertedTexture);
+                DestroyImmediate(convertedTexture);
+
+                EditorPrefs.SetString(SAVE_PATH_PREF_KEY, savePath);
+            }
+        }
+
+        private void DrawImagePreview()
+        {
+            GUILayout.Label("Image Preview", EditorStyles.boldLabel);
+
+            int maxPreviewDimension = Mathf.Min(Mathf.Max(textureWidth, textureHeight), MAX_PREVIEW_DIMENSION);
+            float previewWidth = ImageAspectRatio >= 1f ? maxPreviewDimension : maxPreviewDimension * ImageAspectRatio;
+            float previewHeight = ImageAspectRatio >= 1f ? maxPreviewDimension / ImageAspectRatio : maxPreviewDimension;
+
+            Rect previewTextureRect = new Rect(
+                WINDOW_HORIZONTAL_BUFFER * TEXTURE_HORIZONTAL_OFFSET_SPLIT + (MAX_PREVIEW_DIMENSION - previewWidth) / 2,
+                WINDOW_VERTICAL_BUFFER * TEXTURE_VERTICAL_OFFSET_SPLIT,
+                previewWidth,
+                previewHeight);
+
+            EditorGUI.DrawPreviewTexture(previewTextureRect, renderTexture);
+
+            EditorGUILayout.Space();
+            DrawAlphaPreviewToggle();
+
+            if (showAlpha)
+            {
+                DrawAlphaPreview();
+            }
+        }
+
+        private void DrawAlphaPreviewToggle()
+        {
+            const float SHOW_LABEL_WIDTH = 70f;
+            const float SHOW_TOGGLE_WIDTH = 20f;
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUIStyle labelStyle = new GUIStyle(EditorStyles.label)
+                {
+                    normal =
+                    {
+                        textColor = renderCamera.clearFlags == CameraClearFlags.Depth ? Color.white : Color.black
+                    }
+                };
+
+                GUILayout.FlexibleSpace();
+                EditorGUILayout.LabelField("Show Alpha", labelStyle, GUILayout.MaxWidth(SHOW_LABEL_WIDTH));
+                showAlpha = EditorGUILayout.Toggle(showAlpha, GUILayout.MaxWidth(SHOW_TOGGLE_WIDTH));
+            }
+        }
+
+        private void DrawAlphaPreview()
+        {
+            const int ALPHA_PREVIEW_DIMENSION = 150;
+            const uint BORDER_WIDTH = 1;
+
+            float previewWidth = ImageAspectRatio >= 1f ? ALPHA_PREVIEW_DIMENSION : ALPHA_PREVIEW_DIMENSION * ImageAspectRatio;
+            float previewHeight = ImageAspectRatio >= 1f ? ALPHA_PREVIEW_DIMENSION / ImageAspectRatio : ALPHA_PREVIEW_DIMENSION;
+
+            Rect previewTextureRect = new Rect(
+                WINDOW_HORIZONTAL_BUFFER * TEXTURE_HORIZONTAL_OFFSET_SPLIT + MAX_PREVIEW_DIMENSION * 0.7f,
+                WINDOW_VERTICAL_BUFFER * TEXTURE_VERTICAL_OFFSET_SPLIT + MAX_PREVIEW_DIMENSION * 0.7f,
+                previewWidth,
+                previewHeight);
+            previewTextureRect.x += ALPHA_PREVIEW_DIMENSION - previewWidth;
+            previewTextureRect.y += ALPHA_PREVIEW_DIMENSION - previewHeight;
+
+            Rect previewTextureBorderRect = CreateBorderRect(previewTextureRect, BORDER_WIDTH);
+
+            EditorGUI.DrawRect(previewTextureBorderRect, Color.magenta);
+            EditorGUI.DrawTextureAlpha(previewTextureRect, renderTexture, ScaleMode.ScaleToFit);
+        }
+
+        private Rect CreateBorderRect(Rect originalRect, uint borderWidth)
+        {
+            Debug.Assert(borderWidth > 0, "Border width must be at least 1 pixel");
+
+            Rect borderRect = new Rect(originalRect);
+            borderRect.x -= borderWidth;
+            borderRect.y -= borderWidth;
+            borderRect.width += borderWidth * 2;
+            borderRect.height += borderWidth * 2;
+
+            return borderRect;
+        }
+
+        private Texture2D ConvertToTexture2D(RenderTexture renderTexture)
+        {
+            RenderTexture previousRenderTexture = RenderTexture.active;
+            RenderTexture.active = renderTexture;
+
+            Texture2D savableTexture = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+            savableTexture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+            savableTexture.Apply();
+
+            RenderTexture.active = previousRenderTexture;
+
+            return savableTexture;
+        }
+
+        private void SaveTexture(Texture2D texture)
+        {
+            byte[] encodedImageData;
+
+            switch (imageFormat)
+            {
+                case ImageFormat.jpg:
+                    encodedImageData = texture.EncodeToJPG();
+                    break;
+                case ImageFormat.png:
+                    encodedImageData = texture.EncodeToPNG();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            savePath = string.IsNullOrWhiteSpace(savePath) ? $"{Application.dataPath}/{DEFAULT_FILENAME}.{imageFormat}" : savePath;
+
+            File.WriteAllBytes(savePath, encodedImageData);
+        }
+
+        private enum ImageFormat
+        {
+            jpg,
+            png
+        }
+    }
+}
